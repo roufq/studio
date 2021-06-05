5a29c8fc6749cf159ed122b40527a6c916a8fc14
Rouf1603 <roufmawanto963@gmail.com>
2021-06-05 23:36:25 +0700

    update

--- resources/views/admin/schedule/create.blade.php
+++ resources/views/admin/schedule/create.blade.php
@@ -57,7 +57,7 @@
                             </div>
                             <div class="form-group form-float">
                                 <div class="form-line">
-                                    <input type="datetime-local" class="form-control" id="end" name="end"required>
+                                    <input type="text" class="form-control" id="end" name="end"required>
                                     <label class="form-label">end</label>
                                 </div>
                             </div>
@@ -131,41 +131,18 @@
 <script type="text/javascript">
 $(document).ready(function(){
   $("input").change(function(){
-      var waktuMulai = $('#start').val(),
-          waktuSelesai = $('#end').val(),
-          hours = waktuSelesai.split(':')[0] - waktuMulai.split(':')[0],
-          minutes = waktuSelesai.split(':')[1] - waktuMulai.split(':')[1];
-
-         //create time format          
-         var timeStart = new Date('#start' + waktuMulai).getHours();
-         var timeEnd = new Date('#end' + waktuSelesai).getHours();
-
-         var difference = timeEnd - timeStart;             
-
-         difference = difference / 60 / 60 / 1000;
+    var waktuMulai = $('#start').val(),
+			waktuSelesai = $('#end').val(),
+			minutes = movie_duration;
+			if (minutes <= 0) {
+				alert('Movie minute not defind');
+			}
+			var startDate = new Date(waktuMulai).getTime();
+			var endDate = new Date(startDate + minutes*60000).toString('yyyy-MM-ddThh:mm');
+			$('#end').val(endDate);
 
 
-
-        if ( ($("#start").val() != "") && ($("#end").val() != "")) {
-            var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-            var firstDate = new Date($("#start").val());
-            var secondDate = new Date($("#end").val());
-            var diffDays = Math.round(Math.round((secondDate.getTime() - firstDate.getTime()) / (oneDay)));
-        }
-        if (waktuMulai <= '#start' && waktuSelesai >= '#end'){
-            a = 1;
-        }else {
-            a = 0;
-        }
-        minutes = minutes.toString().length<2?'0'+minutes:minutes;
-        if(minutes<0){ 
-            hours--;
-            minutes = 60 + minutes;        
-        }
-        hours = hours.toString().length<2?'0'+hours:hours;
-        
-        
-        $('#finish').val(diffDays+':'+difference+ ':' + minutes);
+          
   });
 });
 
